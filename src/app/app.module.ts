1e6c711fd3b43f66cacbbada47c0587977ef8a0c
fquinteiro <fquinteiro@gmail.com>
2019-10-19 13:37:08 -0300

    Creating initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { InvestmentsComponent } from './investments/investments.component';
+import { HeaderComponent } from './generics/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    InvestmentsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
